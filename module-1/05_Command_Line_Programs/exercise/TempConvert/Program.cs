ecd71bea11d8bb1cbe5352dd2985171831b6027c
Jake Nelson <jakenelson1050@gmail.com>
2022-01-24 11:51:15 -0500

    updating

--- module-1/05_Command_Line_Programs/exercise/TempConvert/Program.cs
+++ module-1/05_Command_Line_Programs/exercise/TempConvert/Program.cs
@@ -6,7 +6,22 @@ namespace TempConvert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a temperature value to convert:");
+            int tempInput = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Is the temperature in (C)elsius or (F)ahrenheit");
+            string cOrF = Console.ReadLine();
+
+            if (cOrF == "C") 
+            {
+                int nowFahrenheit = (int)(tempInput * 1.8 + 32);
+                Console.WriteLine("Your temperature in fahrenheit is " + nowFahrenheit);
+            }
+            else if (cOrF == "F")
+            {
+                int nowCelsius = (int)(((tempInput - 32) / 1.8));
+                Console.WriteLine("Your temperature in fahrenheit is " + nowCelsius + " degrees");
+            }
+            
         }
     }
 }
