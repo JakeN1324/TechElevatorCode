131bed96085e30a3cbf1b5e8395b24df39d86095
Jake Nelson <jakenelson1050@gmail.com>
2022-02-02 19:24:41 -0500

    Finished mangaing inheritance exercise

---
+++ module-1/13_Managing_Inheritance/exercise/Exercises/SquareWall.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises
+{
+    class SquareWall : RectangleWall
+    {
+        public int SideLength { get; }
+        public SquareWall(string name, string color, int sideLength) : base(name, color, sideLength, sideLength)
+        {
+            
+        }
+
+        public override int GetArea()
+        {
+            return Length * Height;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Length}x{Height}) square";
+        }
+
+
+    }
+}
