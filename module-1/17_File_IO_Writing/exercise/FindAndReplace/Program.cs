5b8dfb610a6859be4655c008f65728df209f1bfe
Jake Nelson <jakenelson1050@gmail.com>
2022-02-08 18:18:43 -0500

    Finished File I/O writing exercise

--- module-1/17_File_IO_Writing/exercise/FindAndReplace/Program.cs
+++ module-1/17_File_IO_Writing/exercise/FindAndReplace/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.IO;
 
 namespace FindAndReplace
 {
     public class Program
     {
-		public static void Main(string[] args)
-		{
-            Console.WriteLine("Hello World!");
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is the search word?");
+            string searchWord = Console.ReadLine();
+
+            Console.WriteLine("What is the replacement word?");
+            string replacementWord = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("What is the source file path?");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured while reading the file.");
+                Console.WriteLine(e.Message);
+            }
+            string sourceFile = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("What is the destination file path?");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured while reading the file.");
+                Console.WriteLine(e.Message);
+            }
+            string destinationFile = Console.ReadLine();
+
+            using (StreamReader sr = new StreamReader(sourceFile))
+            {
+                using (StreamWriter sw = new StreamWriter(destinationFile))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string newLine = line.Replace(searchWord, replacementWord);
+                        sw.WriteLine(newLine);
+                    }
+                }
+            }
+
         }
     }
 }
--- module-1/17_File_IO_Writing/exercise/FizzWriter/Program.cs
+++ module-1/17_File_IO_Writing/exercise/FizzWriter/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
 namespace FizzWriter
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+            Console.WriteLine("What is the destination file path?");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("There was an error reading the file path.");
+                Console.WriteLine(e.Message);
+            }
+            string destinationFile = Console.ReadLine();
+
+            List<double> numberList = new List<double>();
+            for (int i = 1; i < 301; i++)
+            {
+                numberList.Add(i);
+            }
+            
+            using (StreamWriter sw = new StreamWriter(destinationFile))
+            {
+                foreach(double number in numberList)
+                {
+                    if (number % 3 == 0 && number % 5 == 0)
+                    {
+                        sw.WriteLine("FizzBuzz");
+                    }
+                    else if (number % 3 == 0)
+                    {
+                        sw.WriteLine("Fizz");
+                    }
+                    else if (number % 5 == 0)
+                    {
+                        sw.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        sw.WriteLine(number);
+                    }
+                }
+            }
+            
+
+
         }
     }
 }
