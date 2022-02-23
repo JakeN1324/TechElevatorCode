baa3b2385ff28160c2f50d613c86e6ac5d7cc8db
Jake Nelson <jakenelson1050@gmail.com>
2022-02-23 00:12:08 -0500

    finished most of DAO exercise

--- module-2/06_Data_Access_and_DAO/exercise/EmployeeProjects/DAO/EmployeeSqlDao.cs
+++ module-2/06_Data_Access_and_DAO/exercise/EmployeeProjects/DAO/EmployeeSqlDao.cs
@@ -16,34 +16,139 @@ namespace EmployeeProjects.DAO
 
         public IList<Employee> GetAllEmployees()
         {
-            return new List<Employee>();
+            List<Employee> employees = new List<Employee>();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM employee", conn);
+
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.EmployeeId = Convert.ToInt32(reader["employee_id"]);
+                    employee.DepartmentId = Convert.ToInt32(reader["department_id"]);
+                    employee.FirstName = Convert.ToString(reader["first_name"]);
+                    employee.LastName = Convert.ToString(reader["last_name"]);
+                    employee.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+                    employee.HireDate = Convert.ToDateTime(reader["hire_date"]);
+                    employees.Add(employee);
+                }
+            }
+
+            return employees;
         }
 
         public IList<Employee> SearchEmployeesByName(string firstNameSearch, string lastNameSearch)
         {
-            return new List<Employee>() { new Employee() };
+            IList<Employee> employees = new List<Employee>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM employee WHERE first_name LIKE '@first_name%' AND last_name LIKE '%@last_name';", conn);
+                cmd.Parameters.AddWithValue("@first_name", firstNameSearch);
+                cmd.Parameters.AddWithValue("@last_name", lastNameSearch);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Employee employee = CreateEmployeeFromReader(reader);
+                    employees.Add(employee);
+                }
+            }
+
+            return employees;
         }
 
         public IList<Employee> GetEmployeesByProjectId(int projectId)
         {
-            return new List<Employee>();
+            IList<Employee> employees = new List<Employee>();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM project p JOIN project_employee pe ON pe.project_id = p.project_id JOIN employee e ON e.employee_id = pe.employee_id WHERE p.project_id = @project_id", conn);
+                cmd.Parameters.AddWithValue("@project_id", projectId);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Employee employee = CreateEmployeeFromReader(reader);
+                    employees.Add(employee);
+                }
+            }
+            return employees;
         }
 
         public void AddEmployeeToProject(int projectId, int employeeId)
         {
 
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO project_employee (project_id, employee_id) OUTPUT INSERTED.project_id VALUES (@project_id, @employee_id) ", conn);
+                cmd.Parameters.AddWithValue("@project_id", projectId);
+                cmd.Parameters.AddWithValue("@employee_id", employeeId);
+                
+
+                cmd.ExecuteNonQuery();
+            }
+
         }
 
         public void RemoveEmployeeFromProject(int projectId, int employeeId)
         {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
 
+                SqlCommand cmd = new SqlCommand("DELETE FROM project_employee WHERE project_id = @project_id AND employee_id = @employee_id", conn);
+                
+                cmd.Parameters.AddWithValue("@project_id", projectId);
+                cmd.Parameters.AddWithValue("@employee_id", employeeId);
+
+
+                cmd.ExecuteNonQuery();
+                
+            }
         }
 
         public IList<Employee> GetEmployeesWithoutProjects()
         {
-            return new List<Employee>();
+            IList<Employee> employees = new List<Employee>();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM employee e LEFT JOIN project_employee pe ON pe.employee_id = e.employee_id WHERE pe.project_id IS NULL", conn);
+                
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Employee employee = CreateEmployeeFromReader(reader);
+                    employees.Add(employee);
+                }
+            }
+            return employees;
         }
 
+        private Employee CreateEmployeeFromReader(SqlDataReader reader)
+        {
+            Employee employee = new Employee();
+            employee.EmployeeId = Convert.ToInt32(reader["employee_id"]);
+            employee.DepartmentId = Convert.ToInt32(reader["department_id"]);
+            employee.FirstName = Convert.ToString(reader["first_name"]);
+            employee.LastName = Convert.ToString(reader["last_name"]);
+            employee.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+            employee.HireDate = Convert.ToDateTime(reader["hire_date"]);
 
+            return employee;
+        }
     }
 }
